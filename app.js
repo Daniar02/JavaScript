c073dea43cde970bb29dda56faee5977bc9895b3
Daniar02 <abykeev02@icloud.com>
2023-01-04 21:33:42 +0600

    Проверка робота

--- app.js
+++ app.js
@@ -234,4 +234,30 @@
 // } else {
 //     console.log(`Иди ты пожалуйста ${age} ${age - 18}`)
 // }
-
+// Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?".
+// Если ответ верен выведите в консоле "Успех", если нет -"Вы робот!"
+// а если он введёт "Я не робот", то тоже "Успех"
+const res =prompt('Сколько будет 7 + или - 15?');
+switch(true) {
+    case res === 'Я не робот':
+    case Number(res) === 22:
+    case Number(res) === -8:
+        console.log('Успех');
+        break;
+        default:
+            console.log('Вы робот!');
+}
+if (res === 'Я не робот') {
+    console.log('Успех');
+} else {
+    const resNum = Number(res);
+    switch(resNum) {
+        case 22:
+        case -8:
+        case 8:
+            console.log('успех');
+            break;
+        default:
+            console.log('Вы робот!');
+    }
+}
