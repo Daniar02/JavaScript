b2fbbcddbbe7e0e309725233a5e1999df07b8caf
Daniar02 <abykeev02@icloud.com>
2023-01-04 21:33:42 +0600

    Проверка робота

--- app.js
+++ app.js
@@ -237,27 +237,47 @@
 // Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?".
 // Если ответ верен выведите в консоле "Успех", если нет -"Вы робот!"
 // а если он введёт "Я не робот", то тоже "Успех"
-const res =prompt('Сколько будет 7 + или - 15?');
-switch(true) {
-    case res === 'Я не робот':
-    case Number(res) === 22:
-    case Number(res) === -8:
-        console.log('Успех');
-        break;
-        default:
-            console.log('Вы робот!');
-}
-if (res === 'Я не робот') {
-    console.log('Успех');
-} else {
-    const resNum = Number(res);
-    switch(resNum) {
-        case 22:
-        case -8:
-        case 8:
-            console.log('успех');
-            break;
-        default:
-            console.log('Вы робот!');
-    }
-}
+// const res = prompt('Сколько будет 7 + или - 15?');
+// switch(true) {
+//     case res === 'Я не робот':
+//     case Number(res) === 22:
+//     case Number(res) === -8:
+//         console.log('Успех');
+//         break;
+//         default:
+//             console.log('Вы робот!');
+// }
+// if (res === 'Я не робот') {
+//     console.log('Успех');
+// } else {
+//     const resNum = Number(res);
+//     switch(resNum) {
+//         case 22:
+//         case -8:
+//         case 8:
+//             console.log('успех');
+//             break;
+//         default:
+//             console.log('Вы робот!');
+//     }
+// }
+// const res = prompt('Выборите + или -');
+// switch(true) {
+//     case res === '-':
+//     case Number(res) === 1:
+//         console.log('+')
+//         break;
+//     case res === '2':
+//         console.log('-');
+// }
+// const res1 = + prompt('Выборите цифру');
+// switch(true) {
+//     case res1 === res1:
+// }
+// const res2 =  + prompt('Выборите цифру');
+// switch(true) {
+//     case res2 === res2:
+//     console.log(res1 + res2);
+//     console.log(res1 - res2);
+//     break;
+// }
