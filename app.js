9cd80db0fb3d1408639aed0c02bad11f72bcf701
Daniar02 <abykeev02@icloud.com>
2023-01-07 12:06:22 +0600

    Rest оператор, Функция разбора URL

--- app.js
+++ app.js
@@ -697,3 +697,31 @@
 // const [userName, _, sity] = userData;
 
 // console.log(userName, sity);
+
+// ########################################## Rest оператор ##########################################
+// const data = [1, 2, 3, 4, 5, 6];
+// const [one, two, ... others] = data;
+// console.log(one, two, others);
+
+// ########################################## Функция разбора URL ##########################################
+// Дан произвольный url
+// https://purpleschool.ru/course/javascript'
+// Нужно сделать функцию, которая выводит в консоль:
+// Протокол (https)
+// Доменное имя (purpleschool.ru)
+// Путь внутри сайта (/course/javascript)
+const url = "https://purpleschool.ru/course/javascript";
+const url2 = "https://purpleschool..ru/course/javascript";
+function getUrlParts(url) {
+    const [protocol, _, host, ...path] = url.split('/');
+    if (protocol === 'http:' || protocol === 'https:') {
+        if (!host.includes('.')) {
+            return;
+        }
+        console.log(protocol, _, host, path);
+        console.log(`Протокол: ${protocol.split(':')[0]}`);
+        console.log(`Доменное имя ${host}`);
+        console.log(`Путь внутри сайт /${path.join('/')}`)
+    }
+}
+getUrlParts(url2)
