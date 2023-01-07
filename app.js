d34015b43d2772e96435fa44bc244d395b3ed1d3
Daniar02 <abykeev02@icloud.com>
2023-01-07 20:31:18 +0600

    Расчёт итогового баланса

--- app.js
+++ app.js
@@ -834,3 +834,59 @@
 // for (index in arr) {
 //     console.log(index)
 // }
+
+// ########################################## Расчёт итогового баланса ##########################################
+// Есть выгрузка операций пользователя
+// const operations = [1000, -700, 300, -500, 10000];
+// а так же начальный баланс в 100$
+// Необходимо сделать функции расчёта:
+// Итогового баланса
+// Наличия отрицательного баланса (если после очередной операции
+// баланс < 0, то выдавать false)
+// - Расчёта среднего расхода и среднего дохода
+
+const operations = [1000, -700, 300, -500, 10000];
+const startinngBalance = 100;
+
+function getBalance(arrayOfOperations, initailBalance) {
+    let balance = initailBalance;
+    for (const element of arrayOfOperations) {
+        balance += element;
+    }
+    return balance;
+}
+console.log(getBalance(operations, startinngBalance));
+
+    function chekOperations(arrayOfOperations, initailBalance) {
+        let balance = initailBalance;
+        let isOk = true;
+    for (const element of arrayOfOperations) {
+        balance += element;
+        if (balance < 0) {
+            isOk = false;
+            break;
+        }
+    }
+    return isOk
+}
+
+console.log(chekOperations(operations, startinngBalance));
+
+function avarageOperations(arrayOfOperations) {
+    let positiveCount = 0;
+    let positiveSum = 0;
+    let negativeCount = 0;
+    let negativeSum = 0;
+    for (const element of arrayOfOperations) {
+        if (element > 0) {
+            positiveCount++
+            positiveSum += element
+        }
+        if (element < 0) {
+            negativeCount++
+            negativeSum += element
+        }
+    }
+    return [positiveSum / positiveCount, negativeSum / negativeCount];
+}
+console.log(avarageOperations(operations))
