8fe886e0d486639489542466ea727edf483f1e05
Daniar02 <abykeev02@icloud.com>
2023-02-25 23:29:15 +0600

    Быстрое создание массивов

--- js/app.js
+++ js/app.js
@@ -1315,6 +1315,18 @@
 // },0)
 // console.log(allBudget)
 
+// ############################ Быстрое создание массивов ############################
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(new Array(1, 2, 3, 4, 5));
+// const arr2 = new Array(5);
+// console.log(arr2.fill(1, 0, 3));
+// console.log(arr2.fill(2, 3, 5));
+//
+// const arr3 = Array.from({ length: 5}, (cur, i) => i + 1);
+// console.log(arr3);
+
+
 // ############################ Объекты ############################
 
 // const person = {
