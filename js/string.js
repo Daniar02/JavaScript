460ed93885521d9c4e4c69387d825ddf2ba4d9db
Daniar02 <abykeev02@icloud.com>
2023-02-28 18:43:26 +0600

    About the lines in the book

--- js/string.js
+++ js/string.js
@@ -34,71 +34,95 @@
 
 
 /*############################# Упражнение - проверка номера ##############################*/
-/* Проверить является ли это номером телефона Кыргызстана */
-/* верные */
-const num1 = '89103235356';
-const num2 = '+79103235356';
-const num3 = '+7(910)3235356';
-const num4 = '+7(910) 323-53-56';
-const num5 = ' +7(910) 323-53-56 '
-
-/* не верные */
-const num1Error = '89103235';
-const num2Error = '+7d910d323-53-56';
-const num3Error = '9+7103235356';
-const num4Error = '89103g35356';
-
-function isPhoneNumber(num) {
-    num = num.trim();
-    num = num.replace("+7", "8");
-    if (!num.startsWith("8")) {
-        return false;
-    }
-    num = num.replaceAll("(", "");
-    num = num.replaceAll(")", "");
-    num = num.replaceAll(" ", "");
-    num = num.replaceAll("-", "");
-    if (num.length != 11) {
-        return false;
-    }
-    let onlyNumber = true;
-    for (const char of num) {
-        if (isNaN(Number(char))) {
-            onlyNumber = false;
-            break;
-        }
-    }
-    return onlyNumber;
-}
-
-console.log(isPhoneNumber(num1));
-console.log(isPhoneNumber(num2));
-console.log(isPhoneNumber(num3));
-console.log(isPhoneNumber(num4));
-console.log(isPhoneNumber(num5));
-
-console.log(isPhoneNumber(num1Error));
-console.log(isPhoneNumber(num2Error));
-console.log(isPhoneNumber(num3Error));
-console.log(isPhoneNumber(num4Error));
+// /* Проверить является ли это номером телефона Кыргызстана */
+// /* верные */
+// const num1 = '0709785590';
+// const num2 = '+7709785590';
+// const num3 = '+7(709)785590';
+// const num4 = '+7(709) 78-55-90';
+// const num5 = ' +7(709) 785-590 '
+//
+// /* не верные */
+// const num1Error = '89103235';
+// const num2Error = '+7d910d323-53-56';
+// const num3Error = '9+7103235356';
+// const num4Error = '89103g35356';
+//
+// function isPhoneNumber(num) {
+//     num = num.trim();
+//     num = num.replace("+7", "0");
+//     if (!num.startsWith("0")) {
+//         return false;
+//     }
+//     num = num.replaceAll("(", "");
+//     num = num.replaceAll(")", "");
+//     num = num.replaceAll(" ", "");
+//     num = num.replaceAll("-", "");
+//     if (num.length != 10) {
+//         return false;
+//     }
+//     let onlyNumber = true;
+//     for (const char of num) {
+//         if (isNaN(Number(char))) {
+//             onlyNumber = false;
+//             break;
+//         }
+//     }
+//     return onlyNumber;
+// }
+// // true
+// console.log(isPhoneNumber(num1));
+// console.log(isPhoneNumber(num2));
+// console.log(isPhoneNumber(num3));
+// console.log(isPhoneNumber(num4));
+// console.log(isPhoneNumber(num5));
+// // false
+// console.log(isPhoneNumber(num1Error));
+// console.log(isPhoneNumber(num2Error));
+// console.log(isPhoneNumber(num3Error));
+// console.log(isPhoneNumber(num4Error));
 
 /*############################# Строки и массивы ##############################*/
-const userFullName = "Вася Пупкин Васильевич";
-console.log(userFullName.split(" "));
-const [firstName, familyName, lastName] = userFullName;
-console.log(firstName);
-console.log(familyName);
-console.log(lastName);
-const arr = ["Ты", "знаешь", "JS"];
-console.log(arr.join(" "));
+// const userFullName = "Вася Пупкин Васильевич";
+// console.log(userFullName.split(" "));
+// const [firstName, familyName, lastName] = userFullName;
+// console.log(firstName);
+// console.log(familyName);
+// console.log(lastName);
+// const arr = ["Ты", "знаешь", "JS"];
+// console.log(arr.join(" "));
 
 /*############################# Дополнение строк ##############################*/
-const film = "Звёздные Воины ";
-console.log(film.padStart(50, "*"));
-console.log(film.padEnd(50));
-console.log(film.repeat(10));
+// const film = "Звёздные Воины ";
+// console.log(film.padStart(50, "*"));
+// console.log(film.padEnd(50));
+// console.log(film.repeat(10));
 
 /*############################# Упражнение - маскировка карты ##############################*/
-/* Замаскировать всё, кроме последних 4х символов */
-const card = "2324252627282920222323";
-console.log(card.slice(-4).padStart(16, "*"));
+// /* Замаскировать всё, кроме последних 4х символов */
+// const card = "2324252627282920222323";
+// console.log(card.slice(-4).padStart(16, "*"));
+
+/*############################# Про строки в книге ##############################*/
+// let vowels = "aeiou";
+// for (let i = 0; i < vowels.length; i++) {
+//     console.log(vowels[i])
+// }
+
+// // charAt
+// let vowels = "aeiou";
+// console.log(vowels.charAt(2))
+
+// Совмещение (конкатенация) строк
+// let stringA = "abykeev";
+// let stringB = "Daniar";
+// console.log(stringA +" "+ stringB)
+
+// Разделение строки с помощью split
+// let inspirationalQuote = "That which you can tan pan";
+// let splitWords = inspirationalQuote.split(" ");
+// console.log(splitWords.length);
+
+let days = "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday";
+let splitWords = days.split(",");
+console.log(splitWords[6])
