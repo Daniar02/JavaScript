69cfdaf78f514f0f709dc318483dcacf59c63842
Daniar02 <abykeev02@icloud.com>
2023-03-06 23:20:05 +0600

    Object: Exercise - Wallet

--- js/objects.js
+++ js/objects.js
@@ -521,3 +521,47 @@
 // //
 // console.log(user.getFullName("name" , "sf"))
 
+/*###############################  Упражнение - кошелёк. ###############################*/
+/*
+    Реализовать методы увеличения и уменьшения баланса, при котором каждая операция сохраняется
+    в массив operations в виде {reason: 'Оплата налогов', sum: - 100 }.
+    Возвращается true, если успешно и false, если не хватает баланс
+    Так же реализовать метод вывода числа операций по кошельку
+*/
+const wallet = {
+    balance: 0,
+    operations: [],
+    increase: function (sum, reason) {
+        this.balance += sum;
+        this.operations.push({
+            reason: reason,
+            sum: sum
+        })
+        return true
+    },
+    decrease: function (sum, reason) {
+        if (this.balance < sum) {
+            console.log("Недостаточно баланса")
+            return false;
+        }
+        this.balance -= sum
+        this.operations.push({
+            reason: reason,
+            sum: -sum
+        })
+        return true;
+    },
+    getOperationLength: function () {
+        return this.operations.length
+    }
+}
+
+console.log(wallet.increase(1000, "Зарплата"));
+console.log(wallet.getOperationLength());
+console.log(wallet.decrease(2000, "Покупка ноутбука"));
+console.log(wallet.getOperationLength());
+console.log(wallet.decrease(500, "Покупка телефона"));
+console.log(wallet.getOperationLength());
+console.log(wallet.balance);
+console.log(wallet.operations);
+
