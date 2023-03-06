13c8288f0f455de608b3e56b7cf23706cda2ff54
Daniar02 <abykeev02@icloud.com>
2023-03-06 21:07:51 +0600

    Object: Exercise - sorting users.

--- js/app.js
+++ js/app.js
@@ -915,81 +915,6 @@
 // console.log(arr3);
 
 
-// ############################ Объекты ############################
-
-// const person = {
-//     namee: 'Vladilen',
-//     age: 26,
-//     isProgrammer: true,
-//     languages: ['ru', 'en', 'de'],
-//     'complex key': 'Comlex Value',
-//     ['key_' + (1 + 3)]: 'Computed Key',
-//     greet() {
-//         console.log('greet from person');
-//     },
-//     info() {
-//         console.log("this", this);
-//         console.log('Информаци про человека по имени:', this.namee);
-//     }
-// };
-// console.log(person.namee);
-// console.log(person.age);
-// console.log(person['complex key']);
-// console.log(person.languages);
-// console.log(person['key_' + (1 + 3)]);
-
-// person.age++;
-// person.languages.push('by');
-// console.log(person);
-
-// const {namee, age, languages} = person;
-// console.log(namee, age, languages);
-
-// for (const key in person) {
-//     if (person.hasOwnProperty((key))) {
-//         console.log('key:', key);
-//         console.log('value:', person[key]);
-//     }
-// }
-
-// Object.keys(person).forEach((key) => {
-//     console.log('key:', key);
-//     console.log('value:', person[key]);
-// });
-
-// Context
-// person.info();
-//
-// const logger = {
-//     keys(obj) {
-//         console.log('Object Keys:', Object.keys(this));
-//     },
-//     keysAndValues(){
-//         Object.keys(this).forEach(key => {
-//             console.log(`"${key}": ${this[key]}`);
-//         });
-//     },
-//     withParams(top = false, between = false, bottom = false) {
-//         if (top) {
-//             console.log('--------Start-------');
-//         }
-//         Object.keys(this).forEach((key, index, array) => {
-//             console.log(`"${key}": ${this[key]}`);
-//             if (bottom && index !== array.length -1) {
-//                 console.log('--------------');
-//             }
-//         });
-//         if (bottom) {
-//             console.log('------end------');
-//         }
-//     }
-// };
-// // const bound = logger.keys.bind(person);
-// // bound();
-// // logger.keys.call(person);
-// // logger.keysAndValues.call(person);
-// logger.withParams.call(person, true, true , true);
-// logger.withParams.apply(person, [true, true , true]);
 
 // ############################ Асинхронность ############################
 
@@ -1083,9 +1008,3 @@
     jQuery содержит набор функций, которые позволяют найти нужные вам элементы и провести с ними определенные действия
  */
 
-
-
-
-const arr = [[2, 3], [3, 4], [10, 20]];
-const res = arr.flat();
-console.log(res);
--- js/objects.js
+++ js/objects.js
@@ -390,3 +390,92 @@
 // shuffle(shuffleArray);
 //
 // console.log(shuffleArray);
+
+
+// ############################ Объекты ############################
+
+// const person = {
+//     namee: 'Vladilen',
+//     age: 26,
+//     isProgrammer: true,
+//     languages: ['ru', 'en', 'de'],
+//     'complex key': 'Comlex Value',
+//     ['key_' + (1 + 3)]: 'Computed Key',
+//     greet() {
+//         console.log('greet from person');
+//     },
+//     info() {
+//         console.log("this", this);
+//         console.log('Информаци про человека по имени:', this.namee);
+//     }
+// };
+// console.log(person.namee);
+// console.log(person.age);
+// console.log(person['complex key']);
+// console.log(person.languages);
+// console.log(person['key_' + (1 + 3)]);
+
+// person.age++;
+// person.languages.push('by');
+// console.log(person);
+
+// const {namee, age, languages} = person;
+// console.log(namee, age, languages);
+
+// for (const key in person) {
+//     if (person.hasOwnProperty((key))) {
+//         console.log('key:', key);
+//         console.log('value:', person[key]);
+//     }
+// }
+
+// Object.keys(person).forEach((key) => {
+//     console.log('key:', key);
+//     console.log('value:', person[key]);
+// });
+
+// Context
+// person.info();
+//
+// const logger = {
+//     keys(obj) {
+//         console.log('Object Keys:', Object.keys(this));
+//     },
+//     keysAndValues(){
+//         Object.keys(this).forEach(key => {
+//             console.log(`"${key}": ${this[key]}`);
+//         });
+//     },
+//     withParams(top = false, between = false, bottom = false) {
+//         if (top) {
+//             console.log('--------Start-------');
+//         }
+//         Object.keys(this).forEach((key, index, array) => {
+//             console.log(`"${key}": ${this[key]}`);
+//             if (bottom && index !== array.length -1) {
+//                 console.log('--------------');
+//             }
+//         });
+//         if (bottom) {
+//             console.log('------end------');
+//         }
+//     }
+// };
+// // const bound = logger.keys.bind(person);
+// // bound();
+// // logger.keys.call(person);
+// // logger.keysAndValues.call(person);
+// logger.withParams.call(person, true, true , true);
+// logger.withParams.apply(person, [true, true , true]);
+
+/*############################### Упражнение - сортировка пользователей. ###############################*/
+
+const users = [
+    {name: "Вася", age: 30},
+    {name: "Катя", age: 18},
+    {name: "Аня", age: 40},
+    {name: "Петя", age: 25}
+]
+
+console.log(users.sort((a, b) => a.age - b.age))
+
