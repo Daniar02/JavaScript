0e0ae7b1d60bf5f39e03422bd6bd6930ee4a6918
Daniar02 <abykeev02@icloud.com>
2023-03-07 13:34:00 +0600

    Object: Optional chaining

--- js/objects.js
+++ js/objects.js
@@ -630,3 +630,22 @@
 // //     ...additionalData
 // // }
 // console.log(user)
+
+/*###############################  Optional chaining. ###############################*/
+// const cities = {
+//     msk: {
+//         temp: {
+//             celcius: 25
+//         }
+//     },
+//     spb: {
+//
+//     }
+// }
+//
+// const city = "spb"
+// if (cities[city] != undefined && cities[city].temp != undefined) {
+//     console.log(cities[city].temp.celcius)
+// }
+//
+// console.log(cities[city]?.temp?.celcius)
