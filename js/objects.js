ba7751db270320506c50958238e52e8051c1842c
Daniar02 <abykeev02@icloud.com>
2023-03-07 14:36:22 +0600

    Object: Exercise - Warehouse

--- js/objects.js
+++ js/objects.js
@@ -649,3 +649,64 @@
 // }
 //
 // console.log(cities[city]?.temp?.celcius)
+
+/*###############################  Упражнение - склад. ###############################*/
+/*
+    Сделать объект склад с методами добавления на склад, поиска по складку товара и расчёт веса
+*/
+// const warehouse = {
+//     goods: [],
+//     findGoodById: function (id) {
+//         return this.goods.find(g => g.id == id);
+//     },
+//     addGood: function (good) {
+//         const existedGood = this.findGoodById(good.id);
+//         if (existedGood) {
+//             console.log('Этот товар уже есть на складе');
+//             return;
+//         }
+//         this.goods.push(good)
+//     },
+//     getWeightKg: function () {
+//         return this.goods.reduce((acc, el) =>
+//             acc += el.weight?.kg ? el.weight.kg : 0,
+//             0
+//         )
+//     }
+// };
+//
+// /* Товары */
+// const car = {
+//     id: 1,
+//     weight: {
+//         kg: 1000
+//     },
+//     brand: "Ford"
+// };
+//
+// const chair = {
+//     id: 2,
+//     weight: {
+//         kg: 2
+//     },
+// };
+// const parer = {
+//     id: 3,
+//     color: 'red'
+// }
+//
+// warehouse.addGood(car);
+// warehouse.addGood(car);
+// warehouse.addGood(chair);
+// warehouse.addGood(parer);
+// console.log(warehouse.goods);
+// let finDedItem = warehouse.findGoodById(6);
+// console.log(finDedItem);
+// finDedItem = warehouse.findGoodById(1);
+// console.log(finDedItem);
+// const w = warehouse.getWeightKg();
+// console.log(w)
+
+
+
+
