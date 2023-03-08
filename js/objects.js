93c164b3a940ba228d20480b6e4ff212c86fb363
Daniar02 <abykeev02@icloud.com>
2023-03-08 19:23:10 +0600

    Object: Exercise - an object in an object

--- js/objects.js
+++ js/objects.js
@@ -810,3 +810,34 @@
 //
 // user.getUserInfo();
 // user.getUserArrow()
+
+// ########################################## Упражнение - объект в объекте. ##########################################
+/*
+    дополнить объект методами для получения имени:
+    - компании
+    - ceo
+    - сотрудника
+*/
+// const company = {
+//     name: "ООО Агро",
+//     employees: [
+//         {
+//             name: "Света",
+//             getName: function () {
+//                 return this.name
+//             }
+//         }
+//     ],
+//     ceo: {
+//         name: "Вася",
+//         getName: function () {
+//             return this.name
+//         }
+//     },
+//     getName: function () {
+//         return this.name
+//     }
+// }
+// console.log(company.getName());
+// console.log(company.ceo.getName());
+// console.log(company.employees.map(employee => employee.getName()));
