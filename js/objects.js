36ce606da67b14c67a6947357229b3cb31c7d0e3
Daniar02 <abykeev02@icloud.com>
2023-03-09 11:51:25 +0600

    Object: Bind.

--- js/objects.js
+++ js/objects.js
@@ -889,3 +889,25 @@
 //
 // addDamageFunc.apply(audi, ["Капот", 1]);
 // addDamageFunc.apply(bmw, ["Бампер", 2]);
+
+// ########################################## Bind. ##########################################
+// const audi = {
+//     make: "Audi",
+//     model: "A3",
+//     damages: []
+// };
+//
+// const carManipulation = {
+//     addDamage(part, rate) {
+//         this.damages.push({part, rate});
+//         console.log(`Добавить повреждение на ${this.make} ${this.model}`)
+//     }
+// }
+//
+// const addDamageAudi = carManipulation.addDamage.bind(audi);
+// addDamageAudi("Крыло", 3);
+//
+// const addDamageAudiRoof = carManipulation.addDamage.bind(audi, "Крыша");
+// addDamageAudiRoof(5);
+// addDamageAudiRoof(2);
+// console.log(audi);
