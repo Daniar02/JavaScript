d3fb7acc520d01a3d249d9bc684e05156e08f04b
Daniar02 <abykeev02@icloud.com>
2023-03-09 12:07:19 +0600

    Object: Exercise - Managing this.

--- js/objects.js
+++ js/objects.js
@@ -911,3 +911,25 @@
 // addDamageAudiRoof(5);
 // addDamageAudiRoof(2);
 // console.log(audi);
+
+// ########################################## Упражнение - управление this. ##########################################
+/*
+    Создайте объект пользователя с паролем. С помощью функции ниже удалить пароль сделав функцию сброса пароля
+*/
+
+// function removePassword(reset) {
+//     if (reset) {
+//         this.password = undefined;
+//     } else {
+//         this.password = "1";
+//     }
+// };
+//
+// const user = {
+//     login: "ab@da.com",
+//     password: "12345"
+// }
+//
+// const resetUserPassword = removePassword.bind(user, true);
+// resetUserPassword();
+// console.log(user);
