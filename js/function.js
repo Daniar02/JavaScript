f0bb1476d3714c9f067dfc94fc4be8809682f00b
Daniar02 <abykeev02@icloud.com>
2023-03-09 14:42:09 +0600

    Function: Exercise - working with closures

--- js/function.js
+++ js/function.js
@@ -431,3 +431,40 @@
 // const change2 = changeBalance();
 // change2(100);
 // console.dir(change2);
+
+// ########################################## Упражнение - работа с замыканиями. ##########################################
+/*
+    Сделать функцию пользователя, которая берёт за основу userInfo и за счёт замыкания создаёт новый объект.
+    С котором можно работать как user1(). increse (100)
+*/
+// const userInfo = {
+//     balance: 0,
+//     operations: 0,
+//     increase(sum) {
+//         this.balance += sum;
+//         this.operations++
+//     }
+// }
+//
+// function user() {
+//     const userObj = {
+//         balance: 0,
+//         operations: 0,
+//         increase(sum) {
+//             this.balance += sum;
+//             this.operations++
+//         }
+//     };
+//     return function () {
+//         return userObj
+//     }
+// }
+//
+// const user1 = user();
+// user1().increase(100);
+// user1().increase(100);
+// console.log(user1());
+//
+// const user2 = user();
+// user2().increase(100);
+// console.log(user2());
