d5b579a2555ab93fb6b228e03db3d07242245803
Daniar02 <abykeev02@icloud.com>
2023-03-10 15:09:39 +0600

    DOM: Local storage, JSON

--- js/dom.js
+++ js/dom.js
@@ -37,12 +37,18 @@ function submitForm() {
     document.querySelector('.input').value = '';
     // 1) document.querySelector('.notification').style.display = "block";
     document.querySelector('.notification').classList.add('notification_active');
+    /* Упражнение - сохранение JSON. */
+    const textString = JSON.stringify({
+        text: input
+    });
+    localStorage.setItem('text', textString)
+
     // 3) document.querySelector('.notification').classList.remove('notification_active');
-    console.log(document.querySelector('.notification').getAttribute('class'));
+    // console.log(document.querySelector('.notification').getAttribute('class'));
     // document.querySelector('.notification').setAttribute('class', 'notification');
-    document.querySelector('.notification').setAttribute('key', '1');
-    document.querySelector('.notification').setAttribute('user-id', "1");
-    console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
+    // document.querySelector('.notification').setAttribute('key', '1');
+    // document.querySelector('.notification').setAttribute('user-id', "1");
+    // console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
 
 }
 
@@ -77,3 +83,29 @@ function inputChanged(e) {
 // // newElement.innerText = "Кнопка";
 // newElement.innerHTML = `<button class="${panelClass}">${panelText}</button>`
 // document.querySelector('.test').appendChild(newElement);
+
+// ########################################## Local storage. ##########################################
+/*
+    Объекты веб-хранилища localStorage и sessionStorage позволяют хранить пары ключ/значение в браузере
+ */
+// localStorage.setItem('token', "sting");
+// localStorage.setItem('token1', 1);
+// localStorage.setItem('token2', true);
+// const token1 = localStorage.getItem("token1")
+// console.log(typeof token1);
+// localStorage.removeItem('token');
+// localStorage.clear();
+
+// ########################################## JSON. ##########################################
+/*
+    JSON (JavaScript Object Notation) - простой формат обмена данными, удобный для чтения и написания как человеком,
+    так и компьютером. Он основан на подмножестве языка программирования JavaScript, определенного в стандарте
+    ECMA-262 3rd Edition - December 1999. JSON - текстовый формат, полностью независимый от языка реализации,
+    но он использует соглашения, знакомые программистам C-подобных языков, таких как C, C++, C#, Java, JavaScript, Perl,
+    Python и многих других. Эти свойства делают JSON идеальным языком обмена данными.
+*/
+
+// const obj = JSON.parse('{"a": 1}');
+// console.log(obj);
+// const str = JSON.stringify(obj);
+// console.log(str);
