51b672d10e0ded17ef2c524d34016c48c711d01f
Daniar02 <abykeev02@icloud.com>
2023-03-10 12:53:50 +0600

    DOM: Add Setting attributes.

--- js/dom.js
+++ js/dom.js
@@ -35,9 +35,15 @@ function submitForm() {
     }
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
-    // 1) document.querySelector('.notification').style.display = "block"
-    document.querySelector('.notification').classList.add('notification_active')
-    // 3) document.querySelector('.notification').classList.remove('notification_active')
+    // 1) document.querySelector('.notification').style.display = "block";
+    document.querySelector('.notification').classList.add('notification_active');
+    // 3) document.querySelector('.notification').classList.remove('notification_active');
+    console.log(document.querySelector('.notification').getAttribute('class'));
+    document.querySelector('.notification').setAttribute('class', 'notification');
+    document.querySelector('.notification').setAttribute('key', '1');
+    document.querySelector('.notification').setAttribute('user-id', "1");
+    console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
+
 }
 
 // document.querySelector('.input').addEventListener('keydown', (e) => {
