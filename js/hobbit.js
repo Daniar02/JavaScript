d26643183135b4af7dc6147f0be8be7503c32349
Daniar02 <abykeev02@icloud.com>
2023-03-12 23:52:54 +0600

    DOM: Menu operation

--- js/hobbit.js
+++ js/hobbit.js
@@ -2,13 +2,17 @@
 let hobbits = [];
 const HOBBIT_KEY = 'HOBBIT_KEY';
 
-/* utils */
+/* page */
+const page = {
+    menu: document.querySelector('.menu__list')
+}
 
+/* utils */
 function loadData() {
     const hobbitsString = localStorage.getItem(HOBBIT_KEY);
     const hobbitArray = JSON.parse(hobbitsString);
     if (Array.isArray(hobbitArray)) {
-        hobbits = hobbitArray
+        hobbits = hobbitArray;
     }
 }
 
@@ -16,6 +20,41 @@ function saveData() {
     localStorage.setItem(HOBBIT_KEY, JSON.stringify(hobbits));
 }
 
-(() => {
+/* render */
+function rerenderMenu(activeHobbit) {
+    if (!activeHobbit) {
+        return;
+    }
+
+    for (const hobbit of hobbits) {
+        const existed = document.querySelector(`[menu-hobbit-id="${hobbit.id}"]`);
+        if (!existed) {
+            // создание
+            const element = document.createElement('button');
+            element.setAttribute('menu-hobbit-id', hobbit.id);
+            element.classList.add('menu__item');
+            element.innerHTML = `<img src="./images/${hobbit.icon}.svg" alt="${hobbit.name}" />`;
+            if (activeHobbit.id === hobbit.id) {
+                existed.classList.add('menu__item_active');
+            }
+            page.menu.appendChild(element);
+            continue;
+        }
+        if (activeHobbit.id === hobbit.id) {
+            existed.classList.add('menu__item_active');
+        } else {
+            existed.classList.remove('menu__item_active');
+        }
+    }
+}
+
+function rerender(activeHobbitId) {
+    const activeHobbit = hobbits.find(hobbit => hobbit.id === activeHobbitId);
+    rerenderMenu(activeHobbit);
+}
+
+/* init */
+( () => {
     loadData();
-})()
+    rerender(hobbits[0].id);
+})();
