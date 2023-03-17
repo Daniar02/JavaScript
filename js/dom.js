867ae6fcfe55a776a2350d8e8b24d68fc7d39f13
Daniar02 <abykeev02@icloud.com>
2023-03-17 13:17:51 +0600

    DOM: Getting elements from the page and Actions with elements on the page

--- js/dom.js
+++ js/dom.js
@@ -8,6 +8,73 @@
 // console.log(panelText);
 // document.querySelector(".input").value = "Text";
 
+// ##################################  Получение элементов со страницы ##################################
+// const box = document.getElementById('box');
+// console.log(box);
+//
+// const btns = document.getElementsByTagName('button')[1]; //(2-элемент массива)
+// console.log(btns); //[1];(2-элемент массива внутри од-эл[0])
+//
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+//
+// const hearts = document.querySelectorAll('.heart');
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+//
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
+//
+// const oneHeart1 = document.querySelector('div');
+// console.log(oneHeart1);
+
+// ################################## Действия с элементами на странице ##################################
+// const box = document.getElementById('box'),
+//       btns = document.getElementsByTagName('button'),
+//       circles = document.getElementsByClassName('circle'),
+//       wrapper = document.querySelector('.wrapper'),
+//       hearts = wrapper.querySelectorAll('.heart'),
+//       oneHeart = wrapper.querySelector('.heart');
+// // box.style.backgroundColor = 'blue';
+// // box.style.width = '500px';
+// box.style.cssText = "background-Color: blue; width: 500px";
+//
+// btns[2].style.borderRadius = '100%';
+// circles[0].style.backgroundColor = "red";
+//
+// // for (let i = 0; i < hearts.length; i++) {
+// //     hearts[i].style.backgroundColor = "#000";
+// // }
+//
+// // hearts.forEach(item => {
+// //     item.style.backgroundColor = '#000';
+// // });
+//
+// const div = document.createElement('div');
+// // const text = document.createTextNode('Тут был я');
+//
+// div.classList.add('black');
+// document.body.append(div);
+// // wrapper.append(div); // (конец)
+// // wrapper.appendChild(div);
+// // wrapper.prepend(div);// (начало)
+//
+// // hearts[0].before(div);
+// // hearts[2].after(div);
+//
+// // wrapper.insertBefore(div, hearts[1]);
+//
+// // circles[0].remove(); //(удалить)
+// // wrapper.removeChild(hearts[1]);
+//
+// // hearts[0].replaceWith(circles[0]);
+// // wrapper.replaceChild(circles[0],hearts[0]);
+//
+// // div.innerHTML = "<h1>Hello World </h1>";
+// // // div.textContent = "Hello World";
+// div.insertAdjacentHTML("afterend", "<h2>Hello World</h2>");
+
 // ########################################## Обработка нажатий. ##########################################
 // document.querySelector('.button').addEventListener('click', function () {
 //     const input = document.querySelector('.input').value;
