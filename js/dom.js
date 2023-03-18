210f8a366ad33e9e1f10f573a24bfacc8d98011e
Daniar02 <abykeev02@icloud.com>
2023-03-18 21:51:48 +0600

    DOM: Events and their handlers

--- js/dom.js
+++ js/dom.js
@@ -75,6 +75,54 @@
 // // // div.textContent = "Hello World";
 // div.insertAdjacentHTML("afterend", "<h2>Hello World</h2>");
 
+// ########################### События и их обработчики ###########################
+// const btns = document.querySelectorAll('button'),
+//       overlay = document.querySelector('.overlay');
+
+// // btns.onclick = function() {
+// //     alert('Click');
+// // };
+// // btns.onclick = function() {
+// //     alert('Second click');
+// // };
+
+// // btn.addEventListener('click', () => {
+// //     alert('Click');
+// // });
+
+// // let i = 0;
+// const deleteElement = ('mouseenter', (e) => {
+//     console.log(e.target);
+//     console.log(e.type);
+//     // i++;
+//     // if (i == 2) {
+//     //     btn.removeEventListener("click", deleteElement);
+//     // }
+//     // e.target.remove();
+// });
+// // btns.addEventListener("click", deleteElement);
+// // overlay.addEventListener("click", deleteElement);
+
+// btns.forEach(btn => {
+//     btn.addEventListener("click", deleteElement, {once: true});
+// });
+
+// const link = document.querySelector('a');
+// link.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     console.log(event.target);
+// });
+
+
+// // btn.addEventListener('mouseenter', (e) => {
+// //     // alert('Second click');
+
+// //     // console.log(e.target);
+
+// //     // e.target.remove();
+// //     // console.log('Hover');
+// // });
+
 // ########################################## Обработка нажатий. ##########################################
 // document.querySelector('.button').addEventListener('click', function () {
 //     const input = document.querySelector('.input').value;
