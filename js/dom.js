9542ccdee785ad53f8a39782bf6c2673b426b01c
Daniar02 <abykeev02@icloud.com>
2023-03-19 20:46:02 +0600

    DOM: DOM element navigation, data attributes, foroffice advantage

--- js/dom.js
+++ js/dom.js
@@ -123,6 +123,30 @@
 // //     // console.log('Hover');
 // // });
 
+// ########################## Навигация по DOM - элементам, data-атрибуты, преимущество forof ##########################
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+//
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.lastElementChild);
+//
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+//
+// console.log(document.querySelector('[data-current="3"').nextSibling);
+// console.log(document.querySelector('[data-current="3"').previousSibling);
+// console.log(document.querySelector('[data-current="3"').nextElementSibling);
+//
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == "#text") {
+//         continue;
+//     }
+//     console.log(node);
+// }
+
 // ########################################## Обработка нажатий. ##########################################
 // document.querySelector('.button').addEventListener('click', function () {
 //     const input = document.querySelector('.input').value;
