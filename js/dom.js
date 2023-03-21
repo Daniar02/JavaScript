9197dba8dad5b88faaec5f8bcd3c80f8f905c00c
Daniar02 <abykeev02@icloud.com>
2023-03-21 23:05:21 +0600

    DOM: Events on mobile devices

--- js/dom.js
+++ js/dom.js
@@ -344,3 +344,37 @@ console.log(getTotalProgressByIteration(students));
 // const result = getTotalProgressByRecursion(students);
 
 // console.log(result[0]/result[1]);
+
+// ###################################### События на мобильных устройствах ######################################
+// // touchstart
+// // touchmove
+// // touchend
+// // touchenter
+// // touchleave
+// // touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('start');
+        console.log(e.targetTouches);
+    });
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('End');
+    // });
+});
+// // touches
+// // targetTouches
+// // changetTouches
