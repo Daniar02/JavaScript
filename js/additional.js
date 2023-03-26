cd7936b5f95ea444b60bd3a95f61df6b7f1c77c7
Daniar02 <abykeev02@icloud.com>
2023-03-26 15:20:25 +0600

    Additional: Promise (ES6).

--- js/additional.js
+++ js/additional.js
@@ -294,3 +294,52 @@
 // console.log(bigint + BigInt(number));
 // console.log(Number(bigint) + number);
 
+// ################################ Promise (ES6) ################################
+
+// console.log('Запрос данных...');
+
+// const req = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('Подготовка данных...');
+
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+
+//         resolve(product);
+//     },2000);
+// });
+
+// req.then((product) => {
+//     return  new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         },2000);
+//     });
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error("Произошла ошибка ")
+// }).finally(() => {
+//     console.log('Finally')
+// });
+
+// const test = time => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), time);
+//     });
+// };
+
+// test(1000).then(() => console.log("1000 ms"));
+// test(2000).then(() => console.log("2000 ms"));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log("All");
+// });Promise.race([test(1000), test(2000)]).then(() => {
+//     console.log("All");
+// });
