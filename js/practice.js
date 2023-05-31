caeec8bd5ddcb1ae3db15927f97e6f88b76a5e4b
Daniar02 <abykeev02@icloud.com>
2023-05-31 14:43:57 +0600

    feat: The best time to buy stocks #1

--- js/practice.js
+++ js/practice.js
@@ -105,3 +105,28 @@
 // console.log(merge(input1));
 // console.log(merge(input2));
 // console.log(merge(input3));
+
+/*--------------------------- Лучшее время для покупки акций #1 --------------------------*/
+// let prices1 = [7, 1, 5, 3, 6, 4];
+// let prices2 = [7, 6, 4, 3, 1];
+//
+// const maxProfit = (prices) => {
+//     let minPrice = prices[0]
+//     let maxProfit = 0;
+//
+//     for (let i = 0; i < prices.length; i++) {
+//         const current = prices[i];
+//
+//         if (current < minPrice) {
+//             minPrice = current;
+//         }
+//
+//         if (current - minPrice > maxProfit) {
+//             maxProfit = current - minPrice;
+//         }
+//     }
+//     return maxProfit;
+// }
+//
+// console.log(prices1, maxProfit(prices1));
+// console.log(prices2, maxProfit(prices2));
