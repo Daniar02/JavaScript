353a778f46b9788aea4b6f316d44a5efd64d271e
Daniar02 <abykeev02@icloud.com>
2023-05-31 17:08:41 +0600

    feat: Array methods: map, reduce, filter, forEach

---
+++ js/array.js
@@ -0,0 +1,46 @@
+/*--------------------------- Методы массивов: map, reduce, filter, forEach --------------------------*/
+/* forEach */
+
+// const items = ['item1', 'item2', 'item3'] ;
+// const copy = [];
+
+// for (let i = 0; i < items.length; i++) {
+//     copy.push('New' + ' ' + items[i])
+// }
+
+// items.forEach((element, index, array) => {
+//     copy.push(index + ' ' +'New' + ' ' + element + ' ' + array)
+// })
+// console.log(copy)
+
+/* filter */
+
+// let array = [12, 5, 8, 130, 44];
+// let isBigEnough = (elem) => elem > 10
+// let newArray = array.filter(isBigEnough)
+//
+// console.log(newArray)
+
+/* map */
+
+// const array = [1, 3, 2, 4, 10, 22];
+// const newArray = array.map((elem) => elem * 2)
+// console.log(newArray);
+
+/* reduce */
+
+// const array = [1, 2, 3, 4, 5];
+// const newArray = array.reduce((acc, elem) => {
+//     console.log('acc ' + acc + ' Current elem' + elem)
+//     return acc + elem
+// }, 10)
+//
+// console.log(newArray);
+
+// const array = ['apple', 'banana', 'peach', 'orange'];
+// let fruits = array.reduce((acc, elem) => {
+//     acc[elem] = 1
+//     return acc
+// }, {})
+//
+// console.log(fruits)
