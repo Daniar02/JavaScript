40df5904904fc8cca0475a78cf3b452bf3725114
Daniar02 <abykeev02@icloud.com>
2023-06-01 16:10:54 +0600

    tasks: Какая строка встречается чаще всего

--- js/practice.js
+++ js/practice.js
@@ -217,3 +217,32 @@ console.log(flatten([[1], [[2, 3]], [[[4]]]])) // -> [1, 2, 3, 4];
 // console.log(removeDupes('aabbccdd')) // -> 'abcd'
 // console.log(removeDupes('abcddbca')) // -> 'abcd'
 // console.log(removeDupes('abababcdcdcd')) // -> 'abcd'
+
+/*--------------------------- Какая строка встречается чаще всего --------------------------*/
+/*
+function highestFrequency(array) {
+    const map = {};
+    let maxFreq = 0;
+    let maxFreqStr = array[0]
+
+    for (let i = 0; i < array.length; i++) {
+        const current = array[i];
+
+        if (map[current]) {
+            map[current]++
+        } else {
+            map[current] = 1;
+        }
+
+        if (map[current] > maxFreq) {
+            maxFreq = map[current];
+            maxFreqStr = current
+        }
+    }
+    return maxFreqStr
+}
+console.log(highestFrequency(['a', 'b', 'c', 'c', 'd', 'e'])) // -> c
+console.log(highestFrequency(['abc', 'def', 'abc', 'def', 'abc'])) // -> abc
+console.log(highestFrequency(['abc', 'def'])) // -> abc
+console.log(highestFrequency(['abc', 'abc', 'def', 'def', 'def', 'ghi', 'ghi', 'ghi', 'ghi' ])) // -> ghi
+*/
