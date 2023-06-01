b39b91b17c696bc1c0825cb9c02599bc6dc4027b
Daniar02 <abykeev02@icloud.com>
2023-06-01 17:35:40 +0600

    tasks: Анаграммы

--- js/practice.js
+++ js/practice.js
@@ -291,3 +291,18 @@ console.log(arraySubset([2, 1, 1, 3], [1, 2, 3])) // -> true
 console.log(arraySubset([1, 1, 1, 3], [1, 3, 3])) // -> false
 console.log(arraySubset([1, 2], [1, 2, 3])) // -> false
 */
+
+/*--------------------------- Анаграммы --------------------------*/
+/*
+function allAnagrams(array) {
+    const sorted = array.map(str => str.split('').sort().join(''));
+    for (let i = 1; i < sorted.length; i++) {
+        if (sorted[i] !== sorted[0]) {
+            return false
+        }
+    }
+    return true
+}
+console.log(allAnagrams(['abcd', 'bdac', 'cabd'])) // true
+console.log(allAnagrams(['abcd', 'bdXc', 'cabd'])) // false
+*/
