ad131659747cc4fef77994497bda8eaca808cae9
Daniar02 <abykeev02@icloud.com>
2023-06-01 21:21:53 +0600

    tasks: Алгоритмы

--- js/practice.js
+++ js/practice.js
@@ -306,3 +306,47 @@ function allAnagrams(array) {
 console.log(allAnagrams(['abcd', 'bdac', 'cabd'])) // true
 console.log(allAnagrams(['abcd', 'bdXc', 'cabd'])) // false
 */
+
+/*--------------------------- Алгоритмы --------------------------*/
+// function search(array, target) {
+/* 1) Time: O(n)
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === target) {
+            return i
+        }
+    }
+    return -1
+*/
+
+/* 2) Time: O(log(n))
+    let start = 0;
+    let end = array.length -1;
+
+    if (target < array[start] || target > array[end]) {
+        return -1
+    }
+
+    while (true) {
+        if (target === array[start]) {
+            return start
+        }
+        if (target === array[end]) {
+            return end
+        }
+        if (end - start <= 1) {
+            return -1
+        }
+        const middle = Math.floor((start + end) / 2);
+
+        if (target > array[middle]) {
+            start = middle + 1
+        } else if (target < array[middle]) {
+            end = middle - 1
+        } else  {
+            return middle
+        }
+    }
+ */
+// }
+// console.log(search([1, 3, 6, 13, 17], 13)) // -> 3
+// console.log(search([1, 3, 6, 13, 17], 12)) // -> -1
