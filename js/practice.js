a70c613bc72ff235428ac414c9e7ea0753bdcd6c
Daniar02 <abykeev02@icloud.com>
2023-06-02 12:12:14 +0600

    tasks: Последовательность Фибоначчи

--- js/practice.js
+++ js/practice.js
@@ -414,3 +414,41 @@ console.log(deepEqual('test', 'test!')) // -> false
 console.log(deepEqual()) // -> true
 console.log(deepEqual(25, 'str')) // -> false
 */
+
+/*--------------------------- Последовательность Фибоначчи --------------------------*/
+/* 1)
+function fibonacci(n) {
+    const seq = [1, 1]
+    if (n < 2) {
+        return seq.splice(0, n)
+    }
+    while (seq.length < n) {
+        const last = seq[seq.length -1]
+        const prev = seq[seq.length -2]
+       seq.push(last + prev)
+    }
+    return seq
+}
+*/
+/* 2)
+const fibonacci = (function () {
+    const seq = [1, 1]
+    return function (n) {
+        console.log('Called with ', n)
+        if (seq.length >= n) {
+            console.log('No compute')
+            return seq.slice(0, n)
+        }
+        while (seq.length < n) {
+            const last = seq[seq.length -1]
+            const prev = seq[seq.length -2]
+            seq.push(last + prev)
+            console.log('Pushed: ', seq[seq.length -1])
+        }
+        return seq
+    }
+})()
+*/
+// console.log(fibonacci(10)) // -> [1, 1, 2, 3, 5, 8, 13, 21]
+// console.log(fibonacci(8)) // -> [1, 1, 2, 3, 5, 8, 13, 21]
+// console.log(fibonacci(12)) // -> [1, 1, 2, 3, 5, 8, 13, 21]
