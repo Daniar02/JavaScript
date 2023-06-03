52cfec51038f90c6063fc02eb64f2943e2ceafea
Daniar02 <abykeev02@icloud.com>
2023-06-03 22:09:39 +0600

    tasks: GroupBy

--- js/practice.js
+++ js/practice.js
@@ -479,3 +479,22 @@ console.log(add()()()()(20)(22)) // -> 42
 console.log(add()(20)()(22)) // -> 42
 console.log(add()()()()()(20)()()()(22)) // -> 42
 */
+
+/*--------------------------- GroupBy --------------------------*/
+/*
+function groupBy(array, fn) {
+    return array.reduce((res, current) => {
+        const key = typeof fn === 'function' ? fn(current) : current[fn]
+
+        if (!res[key]) {
+            res[key] = []
+        }
+        res[key].push(current)
+
+        return res
+    }, {})
+}
+
+console.log(groupBy([6.1, 4.2, 6.3], Math.floor)) // -> { '4': [4.2], '6': [6.1, 6.3] }
+console.log(groupBy(['one', 'two', 'three'], 'length')) // -> { '3': ['one', 'two'], '5': ['three'] }
+*/
