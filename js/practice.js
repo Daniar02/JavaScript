afd56a6d144ad37556f45d2fa47af3e817ddcf0b
Daniar02 <abykeev02@icloud.com>
2023-06-30 23:13:19 +0600

    tasks: Написать функцию, которая принимает два аргумента:

--- js/practice.js
+++ js/practice.js
@@ -686,3 +686,35 @@ let max = array.reduce((acc, next) => Math.max(acc, next));
 // };
 //
 // console.log(groupStudents(students));
+
+/*--------------------------- Написать функцию, которая принимает два аргумента: --------------------------*/
+/*
+    массив из уникальных целых чисел и сумму в виде целого числа.
+    Если сумма двух любых чисел массива из аргумента равна числу,
+    которое приходит вторым аргументом, функция должна
+    вернуть новый массив из этих двух чисел в любом порядке.
+    Если решения нет, вернуть пустой массив.
+*/
+// [-1, 11] или [11, -1] - так как -1 + 11 = 10;
+
+// const myNumbers = [3, 5, -4, 8, 11, 1, -1, 6];
+// const sum = 10;
+//
+// const findPairs = (nums, target) => {
+//     for (let i = 0; i < nums.length; i++) {
+//         const numFirst = nums[i];
+//
+//         for (let j = i + 1; j< nums.length; j++) {
+//             const numSecond = nums[j];
+//
+//             if (numFirst + numSecond === target) {
+//                 return [numFirst, numSecond];
+//             }
+//         }
+//     }
+//
+//     return [];
+// }
+//
+// console.log(findPairs(myNumbers, sum));
+
