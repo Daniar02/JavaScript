fdba3ae7155532b61d438499624728485af1ec9e
Daniar02 <abykeev02@icloud.com>
2023-07-20 23:08:15 +0600

    tasks: Правильная последовательность скобок

--- js/practice.js
+++ js/practice.js
@@ -741,3 +741,41 @@ console.log(reversed);
 //     return str.split('').reverse().join('');
 // }
 // console.log(reverseString(myStr));
+
+/*--------------------------- Правильная последовательность скобок --------------------------*/
+// let s1 = '()'; // true;
+// let s2 = '()[]{}'; // true;
+// let s3 = '(]'; // false;
+// let s4 = '{[]}'; // true;
+// let s5 = '([)]'; // false;
+// let s6 = '{[[]{}]}'; // true;
+//
+// function isValid(s) {
+//     let stack = [];
+//     let brackets = {
+//         ')': '(',
+//         '}': '{',
+//         ']': '[',
+//     }
+//
+//     for (let i = 0; i < s.length; i++) {
+//         const current = s[i];
+//         if (isClosedBracket(current)) {
+//             if (brackets[current] !== stack.pop()) return false;
+//         } else {
+//             stack.push(current);
+//         }
+//     }
+//     return stack.length === 0;
+// }
+//
+// function isClosedBracket (ch) {
+//     return [')', '}', ']'].indexOf(ch) > -1;
+// }
+//
+// console.log(s1, isValid(s1));
+// console.log(s2, isValid(s2));
+// console.log(s3, isValid(s3));
+// console.log(s4, isValid(s4));
+// console.log(s5, isValid(s5));
+// console.log(s6, isValid(s6));
